d54973244c71bb13b2aac67338dacbde792f3dc0
Lorenz <lorenzb@student.ethz.ch>
2015-11-08 17:19:18 +0100

    Move copy code into its own function

--- util.go
+++ util.go
@@ -6,6 +6,24 @@ import (
 	"path/filepath"
 )
 
+func copyFile(from, to string) error {
+	inFile, err := os.Open(from)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+	outFile, err := os.OpenFile(to, os.O_CREATE|os.O_WRONLY|os.O_EXCL, 0666)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+	_, err = io.Copy(outFile, inFile)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func duplicateFile(from, to string) error {
 	// First try to symlink; if that doesn't work, fall back to copying
 	absFrom, err := filepath.Abs(from)
@@ -13,18 +31,7 @@ func duplicateFile(from, to string) error {
 		return err
 	}
 	if err := os.Symlink(absFrom, to); err != nil {
-		inFile, err := os.Open(from)
-		if err != nil {
-			return err
-		}
-		defer inFile.Close()
-		outFile, err := os.OpenFile(to, os.O_CREATE|os.O_WRONLY|os.O_EXCL, 0666)
-		if err != nil {
-			return err
-		}
-		defer outFile.Close()
-		_, err = io.Copy(outFile, inFile)
-		if err != nil {
+		if err := copyFile(from, to); err != nil {
 			return err
 		}
 	}
